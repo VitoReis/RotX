e97779a536b061b0487d56c5057e758f7a48f0d4
Vitor Silva Reis <vitorejuvian@hotmail.com>
2022-04-29 18:25:26 -0300

    Adding basic code

---
+++ client.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = 'servername'                           #Inserir IP da máquina
+serverPort = 12000                                  #Inserir porta da máquina
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = input('Input lowercase sentence: ')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From Server: ', modifiedSentence.decode())
+clientSocket.close()
