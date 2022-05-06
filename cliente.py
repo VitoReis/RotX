175c35a8fa594be349888be638e58aa4659cbff4
Vitor Silva Reis <vitorejuvian@hotmail.com>
2022-05-06 19:07:20 -0300

    Updating functions

--- cliente.py
+++ cliente.py
@@ -1,10 +1,54 @@
 from socket import *
-serverName = 'localhost'                           #Inserir IP da máquina
-serverPort = 12000                                  #Inserir porta da máquina
+from struct import *
+import re
+
+def encode(message):
+    i = 0
+    index = 0
+
+    identifier = re.compile("^[a-z][a-z]*?[a-z]$")
+
+    # Aceita apenas menssagens em letras minusculas e sem espaçamentos ou outros caracteres
+    if not re.match(identifier, message):
+        print('ERROR')
+        quit()
+
+    letter = message[0]
+    encoded = ''
+    while i < len(message):
+        asciiNum = ord(letter)              # Retorna o valor ascii do caracter
+
+        if asciiNum + 3 > 122:              # Se passar de z(122) voltamos ao inicio a(97)
+            rest = (asciiNum + 3) % 122
+            asciiNum = 96 + rest
+        else:
+            asciiNum = asciiNum + 3         # Soma o valor ascii na letra
+        encoded += chr(asciiNum)            # Retorna o caracter correspondente a letra original
+        i += 1
+        if i < len(message):                 # Se chegar ao fim da string o mesage[i] não existe
+            letter = message[i]
+    return encoded
+
+
+ip = input('Type the server IP: ')
+port = int(input('Type the server port: '))
+message = encode(input('Input one lowercase word: '))
+size = int(input('Insert the string size: '))
+# ip = 'localhost'
+# port = 12000
+# message = encode('stringdetest')
+# size = 1
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = input('Input lowercase sentence: ')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From Server: ', modifiedSentence.decode())
+clientSocket.connect((ip, port))
+print(f'Sending to server: {message}, {size}')
+clientSocket.send(pack('>I', size))
+clientSocket.send(message.encode())
+decodedMessage = clientSocket.recv(1024).decode()
+print(f'From server: {decodedMessage}')
 clientSocket.close()
+
+#           A fazer:
+# Remover necessidade de .encode() se for obrigatorio
+# Conseguir enviar size em pack('>i', size)
+# Conseguir desligar o servidor se for obrigatorio
--- servidor.py
+++ servidor.py
@@ -1,13 +1,43 @@
 from socket import *
+from struct import *
+import re
+import threading
+import time
 
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('', serverPort))
+def decode(encoded):
+    i = 0
+    index = 0
+    letter = encoded[0]
+    decoded = ''
+
+    while i < len(encoded):
+        asciiNum = ord(letter)              # Retorna o valor ascii do caracter
+        if asciiNum - 3 < 97:               # Se menor que a(97) voltamos ao fim z(122)
+            rest = 97 - (asciiNum - 3)
+            asciiNum = 123 - rest
+        else:
+            asciiNum = asciiNum - 3         # Subtrai o valor ascii na letra
+        decoded += chr(asciiNum)            # Retorna o caracter correspondente a letra codificada
+        i += 1
+        if i < len(encoded):                 # Se chegar ao fim da string o mesage[i] não existe
+            letter = encoded[i]
+    return decoded
+
+
+port = int(input('Insert the port: '))
+# port = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', port))
 serverSocket.listen(1)
 print('The server is ready to receive')
 while True:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-connectionSocket.close()
+    size = connectionSocket.recv(2048)
+    size = unpack('>I', size)[0] - 1
+    print(size)
+    message = connectionSocket.recv(1024).decode()
+    print(f'From client: {message}')
+    decoded = decode(message)
+    print(f'Sending to client: {decoded}')
+    connectionSocket.send(decoded.encode())
+    connectionSocket.close()
